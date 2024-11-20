2bb771d20069285589fda916899589ccdfdeae41
jirbthagoras <hansjabriel@gmail.com>
2024-11-20 19:59:00 +0700

    [+] Adding Necessary Models

---
+++ database/seeders/ExplanationsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ExplanationsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
